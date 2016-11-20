01875959c1df6dc0a15775913599e6d4df43f52c
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2016-11-20 12:04:18 -0500

    Added basic leaderboard support for future uses

---
+++ Source/DarklightProject/DarklightProjectLeaderboard.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DarklightProject.h"
+#include "DarklightProjectLeaderboard.h"
+
+
+
+
+UDarklightProjectLeaderboard::UDarklightProjectLeaderboard()
+{
+	SaveSlotName = TEXT("Leaderboard");
+}
