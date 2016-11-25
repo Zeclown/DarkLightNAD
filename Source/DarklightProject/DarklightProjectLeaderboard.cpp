75ee6b3b2ae1982f33957e83d8889fbe522c0b81
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2016-11-25 17:24:34 -0500

    Added basic UI functionalities -HealthBar -Score

--- Source/DarklightProject/DarklightProjectLeaderboard.cpp
+++ Source/DarklightProject/DarklightProjectLeaderboard.cpp
@@ -5,8 +5,32 @@
 
 
 
+void UDarklightProjectLeaderboard::OrderScores()
+{
+	for (int i = 0; i < ScoreEntries.Num()-1; i++)
+	{
+		if (ScoreEntries[i].Score > ScoreEntries[i + 1].Score)
+		{
+			ScoreEntries.Swap(i, i + 1);
+			i = 0;
+		}
+	}
+}
+bool UDarklightProjectLeaderboard::SubmitScore(int Score)
+{
+	return ScoreEntries.Num() < MAX_NUMBER_OF_ENTRIES || Score < ScoreEntries.Last().Score;
+}
+void UDarklightProjectLeaderboard::AddScore(FString Name, int Score)
+{
+	if (ScoreEntries.Num() < MAX_NUMBER_OF_ENTRIES)
+		ScoreEntries.Add(FLeaderboardEntry(Name, Score));
+	else
+		ScoreEntries[ScoreEntries.Num() - 1] = FLeaderboardEntry(Name, Score);
+	OrderScores();
+}
 
 UDarklightProjectLeaderboard::UDarklightProjectLeaderboard()
 {
 	SaveSlotName = TEXT("Leaderboard");
+	OrderScores();
 }
